4982276de00c692b5d1eb457907b221f9a0e3866
Rituja Dixit <rituja.dixit@xebia.com>
2021-03-19 18:24:40 +0530

    passing data between screens

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function ReviewDetails() {
+export default function ReviewDetails({ navigation }) {
     return (
         <View style={globalStyles.container}>
-            <Text>ReviewDetails Screen</Text>
+            <Text>{navigation.getParam('title')}</Text>
+            <Text>{navigation.getParam('body')}</Text>
+            <Text>{navigation.getParam('rating')}</Text>
         </View>
     );
 }
